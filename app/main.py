95e1d2f65b77d5b789ce533661e9fd4cc5e860ad
108-0 <jwm0325@gmail.com>
2025-03-31 16:06:18 +0900

    장르선택

--- app/main.py
+++ app/main.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
 import pandas as pd
+from resolver import random_items, ramdom_genres_items, ramdom_genres_items_best
+from recommender import item_based_recommendation
 
 app = FastAPI()
-
-def random_items():
-    movies_df = pd.read_csv('data/movies_final.csv')
-    movies_df = movies_df.fillna('')
-    result_items = movies_df.sample(n=10).to_dict("records")
-    return result_items
 
 @app.get("/")
 async def root():
@@ -21,7 +17,18 @@ async def all_movies():
 
 @app.get("/genres/{genre}")
 async def genre_movies(genre: str):
-    return {"result" : f'선택한 장르{genre}'}
+    result = ramdom_genres_items(genre)
+    return {"result" : result}
+
+@app.get("/genresbest/{genre}")
+async def genre_movies_best(genre: str):
+    result = ramdom_genres_items_best(genre)
+    return {"result" : result}
+
+@app.get("/item_based/{item_id}")
+async def item_based(item_id : str):
+    result = item_based_recommendation(item_id)
+    return {"result":result}
 
 if __name__ == '__main__':
     uvicorn.run(app,host='127.0.0.1',port = 8000)
---
+++ app/recommender.py
@@ -0,0 +1,12 @@
+from app.model_train import saved_model_fname, item_fname
+
+saved_model_fname = 'data/fimalized_model.sav'
+data_fname = 'data/ratings.csv'
+item_fname = 'data/movies_final.csv'
+weight = 10
+
+def calculate_item_based(item_id,items):
+    return ""
+
+def item_based_recommendation(item_id):
+    return  "item_based_recommendation"
